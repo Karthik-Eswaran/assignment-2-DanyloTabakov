f86eb357b51db8a3b98ae97b1e740ac5c4a7bc9e
DanyloTabakov <cavabecho48@gmail.com>
2025-05-02 10:50:16 +0200

    Made the assigment

--- assignment.py
+++ assignment.py
@@ -1,34 +1,40 @@
+import os
+import importlib
+from pathlib import Path
+
+
 def read_file(file_path: str) -> str:
-    """
-    Reads the contents of a file and returns it as a string.
-    """
-    raise NotImplementedError()
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return f.read()
 
 
 def write_file(file_path: str, content: str) -> None:
-    """
-    Writes the given content to a file.
-    """
-    raise NotImplementedError()
+    direct = os.path.dirname(file_path)
+    if direct and not os.path.exists(direct):
+        os.makedirs(direct)
+    with open(file_path, 'w', encoding='utf-8') as f:
+        return f.write(content)
 
 
 def list_files_in_directory(directory_path: str) -> list:
-    """
-    Returns a list of files in the specified directory.
-    """
-    raise NotImplementedError()
+    path = Path(directory_path)
+    if not path.is_dir():
+        return []
+    
+    files = []
+    for p in path.iterdir():
+        if p.is_file():
+            files.append(p.name)
+    return files
+
 
 
 def generate_numbers(n: int) -> iter:
-    """
-    Generates a sequence of numbers from 0 to n-1 using an iterator.
-    """
-    raise NotImplementedError()
+    for i in range(n):
+        yield i
 
 
 def use_function_from_module(module_name: str, function_name: str, *args) -> any:
-    """
-    Demonstrates how to import a function from another script (module) and execute it.
-    The module name and function name are passed as strings, along with any arguments for the function.
-    """
-    raise NotImplementedError()
+    mod = importlib.import_module(module_name)
+    f = getattr(mod, function_name)
+    return f(*args)
